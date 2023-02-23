bb549a7d0fbec6d745b847f886dce8ad8d3b753f
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-02-23 19:49:35 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo "Halo博客一键搭建脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/halo-on-replit"
+git clone https://github.com/sxbai/halo-on-replit.git
+mv halo-on-replit/* . && cp -r halo-on-replit/.replit .replit
