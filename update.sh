abb8df5f02fb25fef19b3b7a0b1a38e391ee41fd
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-09 19:04:23 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
-echo "Halo一键更新脚本"
+echo "Halo2.0一键更新脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/halo-on-replit"
-rm -rf halo.jar && wget -O halo.jar https://down.sxbai.repl.co/dl/halo-2.3.0.jar
-echo "更新至Halo2.3.0版本，更新完毕！"
+rm -rf halo.jar && wget -O halo.jar https://down.sxbai.repl.co/dl/halo-2.3.1.jar
+echo "更新至Halo2.3.1版本，更新完毕！"
 echo "点击Run按钮重新启动本项目！！！"
