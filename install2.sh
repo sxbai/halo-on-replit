b90c1960bf47b1a1b3f13b119ebef5eb9d6c51a7
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-13 15:23:23 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,11 +1,10 @@
 echo "Halo博客v2.2.1一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/halo-on-replit"
-git clone https://github.com/sxbai/halo-on-replit.git
-mv halo-on-replit/* . && cp -r halo-on-replit/.replit .replit
+nix-env -iA nixpkgs.wget
+wget https://github.com/sxbai/halo-on-replit/raw/master/install2/.replit
+wget https://github.com/sxbai/halo-on-replit/raw/master/install2/replit.nix
+wget https://github.com/sxbai/halo-on-replit/raw/master/install2/main.sh
 wget -O halo.jar https://down.sxbai.repl.co/dl/halo2.2.1.jar
-bash main.sh && rm install2.sh
-echo "Halo默认用户名admin"
-echo "Halo默认密码需要去.halo2/logs/halo.log日志里寻找"
-echo "Halo默认密码是随机生成的"
-echo "搭建完成，使用愉快！！！"
+echo "首次搭建需要在Secrets设置环境变量"
+echo "设置账号和密码的环境变量，点Run启动Halo项目！！！"
