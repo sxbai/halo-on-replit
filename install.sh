2a28bf0164c527b17abc7a78770b2bc99ab5322d
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-13 15:58:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 echo "Halo博客v1.6.1一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/halo-on-replit"
-git clone https://github.com/sxbai/halo-on-replit.git
-mv halo-on-replit/* . && cp -r halo-on-replit/.replit .replit
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/sxbai/halo-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/sxbai/halo-on-replit/raw/master/replit.nix
+cd ..
+wget -O main.sh https://github.com/sxbai/halo-on-replit/raw/master/main.sh
+cp -r halo-on-replit/.replit .replit
+cp -r halo-on-replit/replit.nix replit.nix
 wget -O halo.jar https://down.sxbai.repl.co/dl/halo1.6.jar
 bash main.sh
-echo "搭建完成，使用愉快！！！"
+echo "搭建完成！！！"
 echo "Halo博客v1.6.1一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/halo-on-replit"
+echo "点击Run按钮，启动Halo博客项目! 使用愉快!!!"
 rm -rf halo-on-replit/ && rm -rf install.sh
