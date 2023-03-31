57f033224bccef8881531b6e4ee037ff1518426f
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-31 14:16:14 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,4 @@
+# 防休眠命令
+while true; do curl -s --user-agent "${UA_Browser}" "https://${REPL_SLUG}.${REPL_OWNER}.repl.co" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 600; done &
+# 启动Halo
 java -jar -Duser.timezone=Asia/Shanghai halo.jar
