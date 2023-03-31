16aa10035c776c238b072518640fb7b03cdddd47
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-03-31 13:04:55 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,4 +1,4 @@
-echo "Halo博客v2.3.2一键搭建脚本"
+echo "Halo博客v2.4.0一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/halo-on-replit"
 nix-env -iA nixpkgs.wget
@@ -8,12 +8,12 @@ wget -O .replit https://github.com/sxbai/halo-on-replit/raw/master/install2/.rep
 wget -O replit.nix https://github.com/sxbai/halo-on-replit/raw/master/install2/replit.nix
 cd ..
 wget -O main.sh https://github.com/sxbai/halo-on-replit/raw/master/install2/main.sh
-wget -O halo.jar https://down2.sxbai.repl.co/halo/v2.3.2/halo-2.3.2.jar
+wget -O halo.jar https://down.sxbai.repl.co/dl/halo-2.4.0.jar
 cp -r build/.replit . && cp -r build/replit.nix .
 rm -rf build/
 echo "首次搭建需要在Secrets设置环境变量"
 echo "设置账号和密码的环境变量，点Run启动Halo项目！！！"
-echo "Halo博客v2.3.2一键搭建脚本"
+echo "Halo博客v2.4.0一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/halo-on-replit"
 rm -rf install2.sh
