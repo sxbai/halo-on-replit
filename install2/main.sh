de2bc4d27a2dae3227cff8e9f2e86d2df3ad3bb8
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-05-27 11:35:32 +0800

    Update main.sh

--- install2/main.sh
+++ install2/main.sh
@@ -1,4 +1,4 @@
 # 防休眠命令
 while true; do curl -s --user-agent "${UA_Browser}" "https://${REPL_SLUG}.${REPL_OWNER}.repl.co" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 600; done &
 # 启动Halo
-java -jar -Duser.timezone=Asia/Shanghai halo.jar --halo_work_dir=${PWD}/.halo2 --halo.external-url=https://${REPL_SLUG}.${REPL_OWNER}.repl.co/ --halo.security.initializer.super-admin-username=${username} --halo.security.initializer.super-admin-password=${password}
+java -jar -Duser.timezone=Asia/Shanghai halo.jar --halo_work_dir=/home/runner/${REPL_SLUG}/.halo2 --halo.external-url=https://${REPL_SLUG}.${REPL_OWNER}.repl.co/ --halo.security.initializer.super-admin-username=${username} --halo.security.initializer.super-admin-password=${password}
